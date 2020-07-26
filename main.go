3e2032ca3fa3c622dbc307bd0eb1c936bcf19525
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2020-07-26 23:57:25 +0900

    完成

--- main.go
+++ main.go
@@ -15,10 +15,15 @@ type templateHandler struct {
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	t.once.Do(func() {
+	t.once.Do(func() { //一度だけ関数を呼び出す
 		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		/*
+		template.Must : テンプレのよみこみ
+		template.ParseFiles :　テンプレのファイルを指定する？
+		filepath.Join : ファイルのパスを結合する
+		*/
 	})
-	print(t.temp1.Execute(w, nil))
+	print(t.temp1.Execute(w, nil)) //wにテンプレをデータとして書き出す
 }
 
 func main(){
