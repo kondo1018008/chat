7fc06c3d21228f4f1f518678a00e8b2521069375
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2020-07-29 09:13:53 +0900

    コマンドライン引数でポートを選択

--- chatAPP/main.go
+++ chatAPP/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
+	"github.com/kondo1018008/chat/trace"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 )
@@ -23,18 +26,23 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 		filepath.Join : ファイルのパスを結合する
 		*/
 	})
-	print(t.temp1.Execute(w, nil)) //wにテンプレをデータとして書き出す
+	print(t.temp1.Execute(w, r)) //wにテンプレをデータとして書き出す
 }
 
 func main(){
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
 	go r.run()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
--- chatAPP/room.go
+++ chatAPP/room.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/kondo1018008/chat/trace"
 	"log"
 	"net/http"
 )
@@ -11,6 +12,7 @@ type room struct{
 	join chan *client
 	leave chan *client
 	clients map[*client]bool
+	tracer trace.Tracer
 }
 
 
@@ -29,14 +31,17 @@ func (r *room) run() {
 		select {
 		case client := <-r.join:
 			r.clients[client] = true
+			r.tracer.Trace("New client joined")
 		case client := <-r.leave:
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					//send message
+					r.tracer.Trace(" -- sent to client")
 				default:
 					//fault send message
 					delete(r.clients, client)
