db90f0dbe1743cbbb3d7e19f2b8d99a1c7ba81a3
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2020-07-31 22:44:44 +0900

    [WIP]コメントアウトで説明を追加

--- chatAPP/room.go
+++ chatAPP/room.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/objx"
 	"log"
 	"net/http"
+	"os"
 )
 
 type room struct{
@@ -17,13 +18,14 @@ type room struct{
 }
 
 
-
+//Javaでいうところのコンストラクタ。値の初期化をしている。
 func newRoom() *room {
 	return &room{
 		forward: make(chan *message),
 		join: make(chan *client),
 		leave: make(chan *client),
 		clients: make(map[*client]bool),
+		tracer: trace.New(os.Stdout),//本ではmain.goで初期化しているが、オリジナルでここで初期化している。
 	}
 }
 
@@ -32,18 +34,18 @@ func (r *room) run() {
 		select {
 		case client := <-r.join:
 			r.clients[client] = true
-			r.tracer.Trace("New client joined")
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			delete(r.clients, client)
 			close(client.send)
-			r.tracer.Trace("Client left")
+			r.tracer.Trace("クライアントが退出しました")
 		case msg := <-r.forward:
 			r.tracer.Trace("メッセージを受信しました：", msg.Message)
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					//send message
-					r.tracer.Trace(" -- sent to client")
+					r.tracer.Trace(" -- クライアントに送信しました")
 				default:
 					//fault send message
 					delete(r.clients, client)
@@ -65,17 +67,19 @@ var upgrader = &websocket.Upgrader{
 }
 
 func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request){
+	//http通信をwebsocketにアップグレードする
 	socket, err := upgrader.Upgrade(w, req,nil)
 	if err != nil {
 		log.Fatal("ServeHTTP:",err)
 		return
 	}
+	//クッキーの取得
 	authCookie, err := req.Cookie("auth")
 	if err != nil{
 		log.Fatal("クッキーの取得に失敗しました：", err)
 		return
 	}
-
+	//クライアントインスタンスの生成（厳密にはインスタンスではない）
 	client := &client{
 		socket: socket,
 		send: make(chan *message, messageBufferSize),
@@ -83,7 +87,7 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request){
 		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
-	defer func() {r.leave <- client}()
+	defer func() {r.leave <- client}()//遅延処理
 	go client.write()
 	client.read()
 }
